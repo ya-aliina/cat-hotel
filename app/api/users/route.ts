2b9763c56c4f9337ef6421e8087b9f1a387ec3d1
ya-aliina <a.s.makovei@gmail.com>
2026-04-10 11:19:25 +0200

    feat: integrate Prisma for database management

---
+++ app/api/users/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { prisma } from '@/prisma/prisma-client';
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json({
+    users,
+  });
+}
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+
+  const user = await prisma.user.create({
+    data,
+  });
+
+  return NextResponse.json({ 
+    user,
+  });
+}
